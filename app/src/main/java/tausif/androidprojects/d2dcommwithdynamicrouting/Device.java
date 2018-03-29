8c04ade1417ac2dcd72aba3752d11df2c5d5177e
Tausif <ctausif@gmail.com>
2018-03-29 02:16:08 -0400

    device type variable added

--- app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Device.java
+++ app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Device.java
@@ -8,12 +8,14 @@ public class Device {
     public String deviceName;
     public String deviceAddress;
     public int selected;
+    public int deviceType;
     public BluetoothDevice bluetoothDevice;
 
-    public Device(String deviceName, String deviceAddress, int selected, BluetoothDevice bluetoothDevice) {
+    public Device(String deviceName, String deviceAddress, int selected, int deviceType, BluetoothDevice bluetoothDevice) {
         this.deviceName = deviceName;
         this.deviceAddress = deviceAddress;
         this.selected = selected;
+        this.deviceType = deviceType;
         this.bluetoothDevice = bluetoothDevice;
     }
 }
