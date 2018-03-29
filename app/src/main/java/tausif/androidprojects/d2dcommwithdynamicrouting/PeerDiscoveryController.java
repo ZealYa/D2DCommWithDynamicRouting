b535f1d609aab01c407587c7b2f4fa028429ef69
Tausif <ctausif@gmail.com>
2018-03-29 08:13:05 -0400

    bluetooth discovery methods added, working on wifi discovery methods

--- app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/PeerDiscoveryController.java
+++ app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/PeerDiscoveryController.java
@@ -1,12 +1,14 @@
 package tausif.androidprojects.d2dcommwithdynamicrouting;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -21,52 +23,62 @@ import java.util.TimerTask;
 
 public class PeerDiscoveryController {
 
-    private Context context;
-    private Timer timer;
+    private HomeActivity homeActivity;
     private int timeSlotCount;
-    private int timeInterval;
     private BluetoothAdapter bluetoothAdapter;
     private ArrayList<Device> bluetoothDevices;
+    private ArrayList<Device> wifiDevices;
     private ArrayList<Device> combinedDeviceList;
+    private IntentFilter intentFilter;
 
-    public PeerDiscoveryController(Context context) {
-        this.context = context;
+    public PeerDiscoveryController(HomeActivity homeActivity) {
+        this.homeActivity = homeActivity;
         configureBluetoothDiscovery();
+        configureWiFiDiscovery();
         timeSlotCount = 0;
-        timeInterval = 30;
-        timer = new Timer();
+        int timeInterval = 15;
+        Timer timer = new Timer();
         timer.scheduleAtFixedRate(new StartStopDiscovery(), 0, timeInterval*1000);
     }
 
     //configure bluetooth device discovery options
-    public void configureBluetoothDiscovery() {
-        context.registerReceiver(broadcastReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+    private void configureBluetoothDiscovery() {
+        intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    //configure wifi device discovery options
+    private void configureWiFiDiscovery() {
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
     }
 
     class StartStopDiscovery extends TimerTask {
         @Override
         public void run() {
+            Log.d("time slot ",String.valueOf(timeSlotCount));
             if (timeSlotCount%2==0) {
+                homeActivity.registerReceiver(broadcastReceiver, intentFilter);
                 bluetoothDevices = new ArrayList<>();
                 //adding up already paired devices`
-                //Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-//                if (pairedDevices.size() > 0) {
-//                    // There are paired devices. Get the name and address of each paired device.
-//                    for (BluetoothDevice device : pairedDevices) {
-//                        Device bluetoothDevice = new Device(device.getName(), device.getAddress(), 0, device);
-//                        bluetoothDevices.add(bluetoothDevice);
-//                    }
-//                }
-                //bluetoothAdapter.startDiscovery();
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        Device bluetoothDevice = new Device(device.getName(), device.getAddress(), 0, 1, device);
+                        bluetoothDevices.add(bluetoothDevice);
+                    }
+                }
+                bluetoothAdapter.startDiscovery();
+
+                wifiDevices = new ArrayList<>();
             }
             else {
-                //bluetoothAdapter.cancelDiscovery();
+                bluetoothAdapter.cancelDiscovery();
+                homeActivity.unregisterReceiver(broadcastReceiver);
                 mergeDeviceLists();
-                for (Device device: combinedDeviceList
-                     ) {
-                    Log.d("device name ",device.deviceName);
-                }
+                homeActivity.peerDiscoveryFinished(combinedDeviceList);
             }
             timeSlotCount++;
         }
@@ -74,8 +86,8 @@ public class PeerDiscoveryController {
 
     private void mergeDeviceLists() {
         combinedDeviceList = new ArrayList<>();
-        combinedDeviceList.add(new Device("Bluetooth Devices " + String.valueOf(timeSlotCount), "", 0, null));
         combinedDeviceList.addAll(bluetoothDevices);
+        combinedDeviceList.addAll(wifiDevices);
     }
 
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
@@ -86,7 +98,7 @@ public class PeerDiscoveryController {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Device bluetoothDevice = new Device(device.getName(), device.getAddress(), 0, device);
+                Device bluetoothDevice = new Device(device.getName(), device.getAddress(), 0, 1, device);
                 int flag = 0;
                 for (Device item:bluetoothDevices) {
                     if (item.deviceAddress.equalsIgnoreCase(bluetoothDevice.deviceAddress)){
@@ -97,6 +109,29 @@ public class PeerDiscoveryController {
                 if (flag == 0){
                     bluetoothDevices.add(bluetoothDevice);
                 }
+            }
+            else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+                    @Override
+                    public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                        for (WifiP2pDevice item : wifiP2pDeviceList.getDeviceList()
+                                ) {
+                            int flag = 0;
+                            for (Device previousDevice: wifiDevices
+                                    ) {
+                                if (previousDevice.deviceAddress.equalsIgnoreCase(item.deviceAddress))
+                                {
+                                    flag = 1;
+                                    break;
+                                }
+                            }
+                            if (flag == 0) {
+                                Device device = new Device(item.deviceName, item.deviceAddress, 0, 0,null);
+                                wifiDevices.add(device);
+                            }
+                        }
+                    }
+                };
             }
         }
     };
