085cf03836d0b528c2cf2ea5bca35712bd9c0b6b
Tausif <ctausif@gmail.com>
2018-07-31 17:33:11 -0400

    constant name changes

--- app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
+++ app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
     public static final int WIFI_DEVICE = 1;
     public static final int BLUETOOTH_DEVICE = 0;
 
-    public static int RTT_PACKET_SIZE = 225;  //in bytes
-    public static final int LOSS_RATIO_PACKET_SIZE = 50;   //in bytes
-    public static final int MAX_LOSS_RATIO_PACKETS_TO_SENT = 100;
+    public static int RTT_PKT_SIZE = 225;  //in bytes
+    public static final int LOSS_RATIO_PKT_SIZE = 50;   //in bytes
+    public static final int MAX_LOSS_RATIO_PKTS = 100;
 
-    public static int timeSlotCount = 0;
+    public static int timeSlotNo = 0;
     public static final int timeSlotLength = 10;    // in seconds
 
     public static final int BT_RSSI = 0;
