bcccee4f8deed1760536f056f7e5074596533857
Tausif <ctausif@gmail.com>
2018-08-06 18:06:12 -0400

    constant name change, new constant added

--- app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
+++ app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
@@ -16,12 +16,14 @@ public class Constants {
     public static final int LOSS_RATIO_PKT_SIZE = 50;   //in bytes
     public static final int MAX_LOSS_RATIO_PKTS = 100;
 
-    public static int timeSlotNo = 0;
     public static final int timeSlotLength = 10;    // in seconds
 
-    public static final int TYPE_RTT = 1;
-    public static final int TYPE_RTT_RET = 2;
-    public static final int TYPE_PKT_LOSS = 3;
+    public static final int RTT = 1;
+    public static final int RTT_RET = 2;
+    public static final int PKT_LOSS = 3;
+    public static final int IP_MAC_SYNC = 4;
 
     public static final int noOfRuns = 60;
+
+    public static final int WIFI_DIRECT_CONNECTION = 0;
 }
