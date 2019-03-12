91d999d03584aa2c2ec6268a9300b1792ae3b309
Tausif <ctausif@gmail.com>
2019-03-12 18:21:47 -0400

    no of experiments changed to 25

--- app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
+++ app/src/main/java/tausif/androidprojects/d2dcommwithdynamicrouting/Constants.java
@@ -33,8 +33,8 @@ public class Constants {
     static final int TCP_THRPT = 108;
 
     static int EXP_NO = 0;
-    static final int MAX_NO_OF_EXPS = 4;
-    static final int MAX_PKT_LOSS_EXPS = 4;
+    static final int MAX_NO_OF_EXPS = 25;
+    static final int MAX_PKT_LOSS_EXPS = 25;
 
     private static final int THROUGHPUT_FILE_LENGTH = 5000000;
 
